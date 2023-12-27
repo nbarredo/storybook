0b7d11e854f0735a3b07ba78a7f0a4e54911070f
Michael Kaplan <michael.kaplan@eversource.com>
2023-12-27 13:38:35 -0500

    Apply Eversource theme to Storybook

---
+++ .storybook/eversource.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Eversource React Component Library',
+  brandUrl: 'https://www.eversource.com',
+  brandImage: 'https://www.eversource.com/content/assets/frontend/images/logo-eversource--main.svg',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import eversource from './eversource';
+
+addons.setConfig({
+  theme: eversource,
+});
