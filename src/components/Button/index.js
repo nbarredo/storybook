c19758c121c0817b12fae672852c42348d61a131
Michael Kaplan <michael.kaplan@eversource.com>
2024-01-02 08:42:43 -0500

    Working on a strategy for custom style override ability and inclusion of Eversource CDN as single source of truth.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button as CarbonButton } from "@carbon/react";
 import PropTypes from "prop-types";
-import styles from "./Button.module.scss";
+// import styles from "./Button.module.scss";
 import "./Button.scss";
 
 function Button ({
@@ -16,7 +16,7 @@ function Button ({
 }) {
   return (
     <CarbonButton
-      className={`${styles.button} ${className || ""}`}
+      className={className || ""}
       disabled={disabled}
       iconDescription='Icon Description'
       kind={kind}
