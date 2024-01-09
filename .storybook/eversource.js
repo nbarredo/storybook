250be1d41132c82abb99d1fcef30740ccd2de252
Michael Kaplan <michael.kaplan@eversource.com>
2024-01-09 11:07:53 -0500

    Move eversource logo to local folder.

--- .storybook/eversource.js
+++ .storybook/eversource.js
@@ -4,6 +4,6 @@ export default create({
   base: 'light',
   brandTitle: 'Eversource React Component Library',
   brandUrl: 'https://www.eversource.com',
-  brandImage: 'https://www.eversource.com/content/assets/frontend/images/logo-eversource--main.svg',
+  brandImage: '/logo-eversource--main.svg',
   brandTarget: '_self',
 });
