799f62bd2e56e077c442347f88cf1434901979f1
Michael Kaplan <michael.kaplan@eversource.com>
2024-01-19 09:09:12 -0500

    Add "open" prop to control initial rendering of accordion.  Fix padding issue when no icon present in the title.

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -6,7 +6,7 @@ import "./Accordion.scss";
 /**
  * An accordion is a vertically stacked list of headers that reveal or hide associated sections of content.
  */
-function Accordion({ children, title, icon }) {
+function Accordion({ children, title, icon, open = false }) {
   const createTitle = () => {
     return (
       <div className="title-container">
@@ -18,7 +18,13 @@ function Accordion({ children, title, icon }) {
 
   return (
     <CarbonAccordion>
-      <AccordionItem title={title && createTitle()}>{children}</AccordionItem>
+      <AccordionItem
+        open={open}
+        title={title && createTitle()}
+        className={icon && "icon"}
+      >
+        {children}
+      </AccordionItem>
     </CarbonAccordion>
   );
 }
@@ -31,5 +37,7 @@ Accordion.propTypes = {
   /** An optional icon to precede the title text */
   icon: PropTypes.node,
   /** The title text for the accordion */
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  /** When set to `true`, the accordion will be open by default. NOTE: Currently, this prop only affects how the accordion will appear on its INITIAL RENDER. The issue is currently being investigated by the dev team.  learn more here: https://github.com/carbon-design-system/carbon/issues/15347  */
+  open: PropTypes.bool
 };
--- src/components/Accordion/Accordion.stories.js
+++ src/components/Accordion/Accordion.stories.js
@@ -13,7 +13,26 @@ export default meta;
 export const Default = {
   args: {
     title: "Where do I find my account number?",
+    open: false,
+    children: (
+      <>
+        <p>
+          Account numbers are on your bill. Contact{" "}
+          <a href="#">customer service</a> for help if you don&#apos;t yet have
+          a bill.
+        </p>
+        <br />
+        <img src={image} />
+      </>
+    )
+  }
+};
+
+export const WithIcon = {
+  args: {
+    title: "Where do I find my account number?",
     icon: <HelpIcon />,
+    open: false,
     children: (
       <>
         <p>
