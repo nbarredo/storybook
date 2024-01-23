add4ef7dd57c003e40fde51c2a37ed9bf95051a7
Michael Kaplan <michael.kaplan@eversource.com>
2024-01-23 14:54:51 -0500

    Add base TextInput component. Begin to customize styling

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { TextInput as CarbonTextInput } from "@carbon/react";
+import PropTypes from "prop-types";
+import "./TextInput.scss";
+
+/**
+ * This components helps to properly organize a group of buttons
+ */
+function TextInput(props) {
+  return <CarbonTextInput {...props} />;
+}
+
+export { TextInput };
+
+TextInput.propTypes = {
+  /** Specify an optional className to be applied to the `<input>` node */
+  className: PropTypes.string,
+  /** Optionally provide the default value of the `<input>` */
+  defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Specify whether the `<input>` should be disabled */
+  disabled: PropTypes.bool,
+  /** Specify whether to display the character counter */
+  enableCounter: PropTypes.bool,
+  /** Provide text that is used alongside the control label for additional help */
+  helperText: PropTypes.node,
+  /** Specify whether you want the underlying label to be visually hidden */
+  hideLabel: PropTypes.bool,
+  /** Specify a custom `id` for the `<input>` */
+  id: PropTypes.string.isRequired,
+  /** `true` to use the inline version. */
+  inline: PropTypes.bool,
+  /** Specify whether the control is currently invalid */
+  invalid: PropTypes.bool,
+  /** Provide the text that is displayed when the control is in an invalid state */
+  invalidText: PropTypes.node,
+  /** Provide the text that will be read by a screen reader when visiting this control */
+  labelText: PropTypes.node,
+  /** Max character count allowed for the input. This is needed in order for enableCounter to display */
+  maxCount: PropTypes.number,
+  /** Optionally provide an `onChange` handler that is called whenever `<input>` is updated */
+  onChange: PropTypes.func,
+  /**
+   * Optionally provide an `onClick` handler that is called whenever the
+   * `<input>` is clicked
+   */
+  onClick: PropTypes.func,
+  /** Specify the placeholder attribute for the `<input>` */
+  placeholder: PropTypes.string,
+  /** Whether the input should be read-only */
+  readOnly: PropTypes.bool,
+  /** Specify the size of the Text Input. Currently supports the following: */
+  size: PropTypes.oneOf(["sm", "md", "lg"]),
+  /** Specify the type of the `<input>` */
+  type: PropTypes.string,
+  /** Specify the value of the `<input>` */
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Specify whether the control is currently in warning state */
+  warn: PropTypes.bool,
+  /** Provide the text that is displayed when the control is in warning state */
+  warnText: PropTypes.node
+};
---
+++ src/components/TextInput/TextInput.stories.js
@@ -0,0 +1,24 @@
+import { TextInput } from "./TextInput";
+
+const meta = {
+  title: "In Progress/TextInput",
+  component: TextInput,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/JewKs8uC7fp1AFHY5kApSG/One-Source-Components?type=design&node-id=429%3A12390&mode=dev"
+    }
+  }
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    placeholder: "Placeholder Text",
+    labelText: "Input Field Label",
+    defaultValue: "Delete this text to view the placeholder",
+    size: "md",
+    id: "test-id"
+  }
+};
