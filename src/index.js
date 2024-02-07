c5ae3bae6e5987d10c1980ecae8e50d5c12d3526
Michael Kaplan <michael.kaplan@eversource.com>
2024-02-07 12:47:45 -0500

    Try adding an entry point into package.json.  Export each component in the entry point.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export * from "./components/Accordion";
+export * from "./components/Button";
+export * from "./components/ButtonSet";
+export * from "./components/Loading";
+export * from "./components/Notification";
+export * from "./components/TextInput";
+export * from "./components/Wizard";
+export * from "./components/Multi Account Dashboard/TotalBalance";
