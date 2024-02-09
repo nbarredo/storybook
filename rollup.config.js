8ab5d4d81575942a3eda04575b8cebad213783fe
Michael Kaplan <michael.kaplan@eversource.com>
2024-02-09 08:48:56 -0500

    Working to resolve package issue where reference to external stylesheet is not generated

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,41 @@
-import babel from 'rollup-plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postscss from 'rollup-plugin-postcss';
-import {terser} from 'rollup-plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from "@rollup/plugin-commonjs";
+import image from "@rollup/plugin-image";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postscss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
-import image from '@rollup/plugin-image';
-
 
 export default [
-    {
-        input: 'src/index.js',
-        output: [
-            {
-                file: pkg.main,
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: pkg.module,
-                format: 'esm',
-                sourcemap: true,                
-            }
-        ],
-        plugins: [
-            peerDepsExternal(),
-            postscss({
-                extract: true,
-                modules: true,
-                use: ["sass"]
-            }),
-            babel({
-               exclude: 'node_modules/**',
-               presets: ['@babel/preset-react'] 
-            }),
-            nodeResolve(),
-            commonjs(),
-            terser(),
-            image()
-        ]
-    }
-]
-
-
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      peerDepsExternal(),
+      postscss({
+        modules: true,
+        use: ["sass"]
+      }),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"]
+      }),
+      nodeResolve(),
+      commonjs(),
+      terser(),
+      image()
+    ]
+  }
+];
