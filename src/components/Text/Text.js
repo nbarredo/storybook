616f6625fc70ff901912cbae69d6f32b474d3145
Michael Kaplan <michael.kaplan@eversource.com>
2024-02-20 11:50:39 -0500

    Develop a Text component that will allow users to access the proper typography styles.

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,7 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { ColorPalette } from "../../utils/ColorPalette";
 import styles from "./Text.module.scss";
 
+/**
+ * This component is used to render properly-styled text.
+ */
 function Text({
   weight = "reg",
   id,
@@ -68,7 +72,7 @@ Text.propTypes = {
   /** Specify a margin to apply below the text. This only applies if inline=false */
   marginBottom: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]),
   /** A string which will determine the styling of the rendered text */
-  color: PropTypes.string,
+  color: PropTypes.oneOf(Object.keys(ColorPalette)),
   /** An optional unique id for the text component.  This is helpful if you want to refer to this element using an `ariaLabelledBy` on some other element */
   id: PropTypes.string,
   /** If your selected variant is an H1, H2, H3, or H4, you can use this optional prop to override the aria heading level. The use case would be:  You run the axe Dev Tools scan and it says, "Page must contain a level 1 header", but the Figma design for your page shows the heading as being an H3 in terms of appearance so you want to tell accessibility devices that no, in fact, the H3 should be thought of as an H1 */
