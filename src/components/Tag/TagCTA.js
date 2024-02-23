c7b7b62473e325a582d71dec22bde50c3f3e8c12
Michael Kaplan <michael.kaplan@eversource.com>
2024-02-23 14:31:05 -0500

    Create new interactive tag variation.  Add styles. In progress.

---
+++ src/components/Tag/TagCTA.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Tag as CarbonTag } from "@carbon/react";
+import PropTypes from "prop-types";
+import styles from "./TagCTA.module.scss";
+
+/**
+ * This interactive tag is used to provide information or important details, and can be used as a CTA or a search filter display.
+ */
+function TagCTA({ text, theme = "default", ...props }) {
+  const themeName = `theme-${theme}`;
+  return (
+    <div className={`${styles["gradient-border"]} ${styles[themeName]}`}>
+      <CarbonTag
+        className={`${styles["tag-cta"]} ${styles[themeName]}`}
+        {...props}
+      >
+        {text}
+      </CarbonTag>
+    </div>
+  );
+}
+
+export { TagCTA };
+
+TagCTA.propTypes = {
+  /** The text content of the tag */
+  text: PropTypes.string,
+  /** Specify what should occur when the tag is clicked */
+  onClick: PropTypes.func,
+  /** Specify the theme of the Text Input. Currently supports the following: */
+  theme: PropTypes.oneOf(["default", "blue"]),
+  /** Add a Material-UI icon to the button.  Search for an icon here:  https://mui.com/material-ui/material-icons/   */
+  renderIcon: PropTypes.oneOfType([PropTypes.func, PropTypes.object])
+};
---
+++ src/components/Tag/TagCTA.stories.js
@@ -0,0 +1,29 @@
+import AddIcon from "@mui/icons-material/Add";
+import { TagCTA } from "./TagCTA";
+
+const meta = {
+  title: "Approved for Use/Tags/Interactive Tag",
+  component: TagCTA,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/JewKs8uC7fp1AFHY5kApSG/One-Source-Components?type=design&node-id=1547-1297&mode=design&t=OsGPiExVR0AP7An0-0"
+    }
+  }
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    text: "ernesto borgnino",
+    theme: "default"
+  }
+};
+
+export const WithIcon = {
+  args: {
+    ...Default.args,
+    renderIcon: AddIcon
+  }
+};
