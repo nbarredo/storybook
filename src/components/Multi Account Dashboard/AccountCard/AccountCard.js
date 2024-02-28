d8f757c75abc8a76c69a30652cb7ea9f972f510d
Michael Kaplan <michael.kaplan@eversource.com>
2024-02-28 13:18:17 -0500

    Work on alignment of items within the card component. In progress.

--- src/components/Multi Account Dashboard/AccountCard/AccountCard.js
+++ src/components/Multi Account Dashboard/AccountCard/AccountCard.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "@carbon/react";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import PropTypes from "prop-types";
 import { Button } from "../../Button/Button";
 import { InlineNotification } from "../../Notification/InlineNotification/InlineNotification";
+import { Tag } from "../../Tag/Tag";
 import { TagCTA } from "../../Tag/TagCTA";
 import { Text } from "../../Text/Text";
 import styles from "./AccountCard.module.scss";
@@ -17,47 +19,54 @@ import { ReactComponent as IconGas } from "./icon_gas.svg";
  */
 function AccountCard({ className, acctType = "electric" }) {
   return (
-    <section className={`${styles.root} ${className}`}>
-      <figure className={`${styles["icon-container"]} ${styles[acctType]}`}>
-        {acctType === "electric" ? <IconElectric /> : <IconGas />}
-      </figure>
-      <header>
-        <Text size="3" weight="semi" inline={true}>
-          {acctType}&nbsp;<span className={styles.pipe}>|</span>&nbsp;acct #
-          1245345
-        </Text>
-        <address>
-          <Text color="gray-60" size="1" weight="reg">
-            124 Main Street NH 120384
+    <section className={`${styles.root} ${className || ""}`}>
+      <article className={styles["content-container"]}>
+        <header>
+          <figure className={`${styles["icon-container"]} ${styles[acctType]}`}>
+            {acctType === "electric" ? <IconElectric /> : <IconGas />}
+          </figure>
+          <Text size="3" weight="semi" inline={true}>
+            {acctType}&nbsp;<span className={styles.pipe}>|</span>&nbsp;acct #
+            1245345
           </Text>
-        </address>
-      </header>
-      <ul className="actions">
-        <li>
-          <TagCTA onClick={() => {}} text="Lorem ipsum" theme="default" />
-        </li>
-        <li>
-          <button>Set Up Autopay</button>
-        </li>
-      </ul>
-      <aside className={styles["payment-area"]}>
-        <div className={styles["total-due"]}>
-          <Text color="gray-60" size="2" weight="semi" inline={true}>
-            <strong>1245</strong>
-            Due 02/06/23
-          </Text>
-        </div>
-        <div className={styles["button-container"]}>
-          <Button kind="tertiary" label="Pay Bill" size="sm" type="button" />
-        </div>
-      </aside>
-      <Button
-        kind="ghost"
-        label="Acct Details"
-        size="sm"
-        type="button"
-        renderIcon={ArrowForwardIcon}
-      />
+          <address>
+            <Text color="gray-60" size="1" weight="reg">
+              124 Main Street NH 120384
+            </Text>
+          </address>
+        </header>
+
+        <ul className={styles.actions}>
+          <li>
+            <TagCTA onClick={() => {}} text="Go Paperless" theme="default" />
+          </li>
+          <li>
+            <Tag icon text="Set Up Autopay" theme="default" />
+          </li>
+        </ul>
+
+        <aside className={styles["payment-area"]}>
+          <div className={styles["total-due"]}>
+            <Text color="gray-60" size="2" weight="semi" inline={true}>
+              <strong>1245</strong>
+              Due 02/06/23
+            </Text>
+          </div>
+          <div className={styles["button-container"]}>
+            <Button kind="tertiary" label="Pay Bill" size="sm" type="button" />
+          </div>
+        </aside>
+
+        <Link
+          href="#"
+          renderIcon={() => (
+            <ArrowForwardIcon sx={{ fontSize: 20 }} aria-label="Arrow Right" />
+          )}
+          className={styles.link}
+        >
+          Acct Details
+        </Link>
+      </article>
       <InlineNotification
         hideCloseButton
         kind="warning"
