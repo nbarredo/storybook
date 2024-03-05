67a174409921a24ef5e5a1ca8e4cbeccee1db5e1
Michael Kaplan <michael.kaplan@eversource.com>
2024-03-05 13:46:04 -0500

    Add Eversource breakpoints to storybook viewport selector.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,38 @@
 import "../src/index.scss";
 
 /** @type { import('@storybook/react').Preview } */
+
+const customViewports = {
+  small: {
+    name: "Smallest viewport size that we support",
+    styles: {
+      width: "475px",
+      height: "963px"
+    }
+  },
+  medium: {
+    name: "Breakpoint Medium (md)",
+    styles: {
+      width: "672px",
+      height: "963px"
+    }
+  },
+  large: {
+    name: "Breakpoint Large (lg)",
+    styles: {
+      width: "1056px",
+      height: "963px"
+    }
+  },
+  xlarge: {
+    name: "Breakpoint Extra-Large (xl)",
+    styles: {
+      width: "1272px",
+      height: "963px"
+    }
+  }
+};
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,6 +41,9 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/i
       }
+    },
+    viewport: {
+      viewports: customViewports
     }
   }
 };
