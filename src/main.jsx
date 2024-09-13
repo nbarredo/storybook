2d870d1828d22c02f209481af7da5b7e8dbaec18
Michael Kaplan <michael.kaplan@eversource.com>
2024-09-13 11:27:06 -0400

    Add missing export for tooltip component

--- src/main.jsx
+++ src/main.jsx
@@ -13,18 +13,19 @@ createRoot(document.getElementById("root")).render(
 import "./index.scss";
 
 export * from "./components/Accordion/Accordion";
+export * from "./components/AccountOverview/AccountInfoCard/AccountInfoCard";
+export * from "./components/AccountOverview/BillingCard/BillingCard";
 export * from "./components/Button/Button";
 export * from "./components/ButtonSet/ButtonSet";
 export * from "./components/LanguageContext/LanguageContext";
 export * from "./components/Loading/Loading";
+export * from "./components/MultiAccountDashboard/AccountCard/AccountCard";
+export * from "./components/MultiAccountDashboard/TotalBalance/TotalBalance";
 export * from "./components/Notification/InlineNotification/InlineNotification";
 export * from "./components/Tag/Tag";
 export * from "./components/Tag/TagCTA";
 export * from "./components/Text/Text";
 export * from "./components/TextInput/TextInput";
+export * from "./components/Tooltip/Tooltip";
 export * from "./components/Wizard/Wizard";
 export * from "./components/Wizard/children";
-export * from "./components/MultiAccountDashboard/TotalBalance/TotalBalance";
-export * from "./components/MultiAccountDashboard/AccountCard/AccountCard";
-export * from "./components/AccountOverview/AccountInfoCard/AccountInfoCard";
-export * from "./components/AccountOverview/BillingCard/BillingCard";
