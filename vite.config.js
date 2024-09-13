b07249c03b39f774e73ca8e94f4248ae3d3c89ee
Michael Kaplan <michael.kaplan@eversource.com>
2024-09-13 15:38:24 -0400

    code cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
-import { defineConfig } from "vite";
 import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
